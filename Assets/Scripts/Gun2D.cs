eed7a50599f87fc730bac58eab5c91d48d4eaee1
Олександр Януш <sashayanus10@gmail.com>
2024-03-18 14:21:57 +0200

    Shoot gun

--- Assets/Scripts/Gun2D.cs
+++ Assets/Scripts/Gun2D.cs
@@ -7,13 +7,42 @@ public class Gun2D : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    public float fireRate = 0.1f;
+    public int maxBullets = 10;
+
+    private bool isShooting = false;
+    private int bulletsFired = 0;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && bulletsFired < maxBullets)
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+            isShooting = true;
+            StartCoroutine(ShootBullets());
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isShooting = false;
+            bulletsFired = 0;
+        }
+    }
+
+    IEnumerator ShootBullets()
+    {
+        while (isShooting && bulletsFired < maxBullets)
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            if (transform.localScale.x < 0)
+            {
+                rb.velocity = -bulletSpawnPoint.right * bulletSpeed;
+            }
+            else
+            {
+                rb.velocity = bulletSpawnPoint.right * bulletSpeed;
+            }
+            bulletsFired++;
+            yield return new WaitForSeconds(fireRate);
         }
     }
 }

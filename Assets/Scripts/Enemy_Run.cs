923df629a075097ded8347f612097b37a1dc6be3
Рома Чорний <roman.cherny1515@gmail.com>
2024-03-21 21:49:55 +0200

    add katana enemy AI (completed), MeleeAttack.unity scene

--- Assets/Scripts/Enemy_Run.cs
+++ Assets/Scripts/Enemy_Run.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class Enemy_Run : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+	public float speed = 2.5f;
+	public float attackRange = 3f;
+	public float sightRange = 0f;
+	Transform player;
+	Rigidbody2D rb;
+	Enemy enemy;
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		rb = animator.GetComponent<Rigidbody2D>();
+		enemy = animator.GetComponent<Enemy>();
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
+	}
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		sightRange = 20f;
+		if (Vector2.Distance(player.position, rb.position) <= sightRange)
+		{
+			animator.SetTrigger("EnemyRun");
+
+			enemy.LookAtPlayer();
+
+			Vector2 target = new Vector2(player.position.x, rb.position.y);
+			Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+			rb.MovePosition(newPos);
+		}
+		else
+		{
+			animator.ResetTrigger("EnemyRun");
+			
+			animator.SetTrigger("EnemyIdle");
+			
+			Debug.Log("Disabled trigger 1 ");
+		}
+
+		if (Vector2.Distance(player.position, rb.position) <= attackRange)
+		{
+			animator.SetTrigger("EnemyKatanaAttack");
+		}
+        
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		animator.ResetTrigger("EnemyKatanaAttack");
+	}
 }
